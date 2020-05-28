8b38417ab75d141c129e08d204398cef8510548a
aaaasmile <testhaus@gmx.at>
2020-05-28 23:47:35 +0200

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aaaasmile/docker-hosts-update
+
+go 1.13
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 	"runtime"
+
+	"github.com/aaaasmile/docker-hosts-update/info"
 )
 
 func main() {
-	log.Println("Welcome to the Host updater")
+	log.Println("Welcome to the Hosts updater")
 	if runtime.GOOS != "windows" {
 		log.Fatal("Windows only")
 	}
@@ -16,5 +18,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println("Now check if the Hosts file need to be updated with ", mmIP)
+	log.Println("Now check if the Hosts file needs to be updated with ", mmIP)
 }
