244607fd073aefd47e0125837dc24b301570e459
aaaasmile <testhaus@gmx.at>
2020-12-17 00:13:23 +0100

    update

--- main.go
+++ main.go
@@ -13,10 +13,15 @@ func main() {
 	log.Println("Welcome to the Hosts updater")
 	var dirout = flag.String("dirout", `c:\windows\system32\drivers\etc`, "Test the Hosts output inside a temp file")
 	var debug = flag.Bool("debug", false, "Turn on debug information")
+	var ver = flag.Bool("ver", false, "Version information")
 	flag.Parse()
 
 	if runtime.GOOS != "windows" {
 		log.Fatal("Windows only")
+	}
+	if *ver {
+		log.Println("Version 0.2.20201217")
+		return
 	}
 
 	mmIP, err := info.CollectContainerHostinfo()
